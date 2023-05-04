33dcbe2eddd09f9d4baa5bf9434afa0ebe8c3303
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-05-04 14:31:18 -0700

    by - Generate issue files

---
+++ issues/generate-issues.js
@@ -0,0 +1,35 @@
+const fs = require("fs")
+const path = require("path")
+
+
+const dir = __dirname
+
+const templates = fs.readdirSync(dir).filter(
+    x=>x.startsWith("issue1")
+)
+
+const objects = [
+    "Book",
+    "Car",
+    "Bike",
+    "Hotel",
+    "Movie"
+]
+
+const original = "Restaurant"
+
+for (let i = 0; i < objects.length; i++) {
+    const obj = objects[i];
+    const num = i + 2
+
+    for(const t of templates) {
+        const content = fs.readFileSync(path.join(dir, t)).toString()
+        const newContent = content.replace(new RegExp(original, "g"), obj)
+        // replace issue1x with issue<num>x
+        const newFileName = t.replace("issue1", `issue${num}`)
+        fs.writeFileSync(path.join(dir, newFileName), newContent)
+    }
+
+}
+
+
