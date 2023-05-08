e31f19d8914ffe79dadf5188b483548321a8075f
Joe Z <joezhang@ucsb.edu>
2023-05-08 15:09:12 -0700

    jz - fixed Hotel.java, HotelRepository.java

--- src/main/java/edu/ucsb/cs156/example/repositories/HotelRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HotelRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UCSBDateRepository extends CrudRepository<Hotel, Long> {
+public interface HotelRepository extends CrudRepository<Hotel, Long> {
 }
