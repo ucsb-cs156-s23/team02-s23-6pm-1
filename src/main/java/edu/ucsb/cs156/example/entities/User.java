16aab122430b961b26e5324382f6762c7b040351
albertyu03 <albertyu@ucsb.edu>
2023-05-09 13:26:55 -0700

    creating book/user entities & bookRepo

--- src/main/java/edu/ucsb/cs156/example/entities/User.java
+++ src/main/java/edu/ucsb/cs156/example/entities/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-
+//comment
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
