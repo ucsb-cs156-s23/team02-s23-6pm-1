c46d380d44d941f6ed8c1ca350b58fb0808278cf
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-05-10 16:08:01 -0700

    by - add restaurant put endpoint for edits

--- src/main/java/edu/ucsb/cs156/example/controllers/RestaurantsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RestaurantsController.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.example.controllers;
 
 import edu.ucsb.cs156.example.entities.Restaurant;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.RestaurantRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Api(description = "Restaurants")
 @RequestMapping("/api/restaurants")
@@ -41,5 +44,24 @@ public class RestaurantsController extends ApiController {
                 .build();
 
         return restaurantRepository.save(restaurant);
+    }
+
+    @ApiOperation(value = "Update a single restaurant")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public Restaurant updateRestaurant(
+            @ApiParam("id") @RequestParam Long id,
+            @RequestBody @Valid Restaurant incoming) {
+
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Restaurant.class, id));
+
+        restaurant.setName(incoming.getName());
+        restaurant.setAddress(incoming.getAddress());
+        restaurant.setDescription(incoming.getDescription());
+
+        restaurantRepository.save(restaurant);
+
+        return restaurant;
     }
 }
--- src/test/java/edu/ucsb/cs156/example/controllers/RestaurantsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/RestaurantsControllerTests.java
@@ -9,18 +9,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = RestaurantsController.class)
@@ -125,5 +127,73 @@ public class RestaurantsControllerTests extends ControllerTestCase {
         String expectedJson = mapper.writeValueAsString(restaurant1);
         String responseString = response.getResponse().getContentAsString();
         assertEquals(expectedJson, responseString);
+    }
+
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void admin_can_edit_an_existing_restaurant() throws Exception {
+        // arrange
+
+        var restaurantOrig = Restaurant.builder()
+                .name("a restaurant")
+                .address("1234 State St")
+                .description("a description")
+                .build();
+
+        var restaurantEdited = Restaurant.builder()
+                .name("another restaurant")
+                .address("5678 State St")
+                .description("another description")
+                .build();
+
+        String requestBody = mapper.writeValueAsString(restaurantEdited);
+
+        when(restaurantRepository.findById(eq(67L))).thenReturn(Optional.of(restaurantOrig));
+
+        // act
+        MvcResult response = mockMvc.perform(
+                        put("/api/restaurants?id=67")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .characterEncoding("utf-8")
+                                .content(requestBody)
+                                .with(csrf()))
+                .andExpect(status().isOk()).andReturn();
+
+        // assert
+        verify(restaurantRepository, times(1)).findById(67L);
+        verify(restaurantRepository, times(1)).save(restaurantEdited); // should be saved with correct user
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals(requestBody, responseString);
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void admin_cannot_edit_restaurant_that_does_not_exist() throws Exception {
+        // arrange
+
+        var restaurantEdited = Restaurant.builder()
+                .name("a restaurant")
+                .address("1234 State St")
+                .description("a description")
+                .build();
+
+        String requestBody = mapper.writeValueAsString(restaurantEdited);
+
+        when(restaurantRepository.findById(eq(67L))).thenReturn(Optional.empty());
+
+        // act
+        MvcResult response = mockMvc.perform(
+                        put("/api/restaurants?id=67")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .characterEncoding("utf-8")
+                                .content(requestBody)
+                                .with(csrf()))
+                .andExpect(status().isNotFound()).andReturn();
+
+        // assert
+        verify(restaurantRepository, times(1)).findById(67L);
+        Map<String, Object> json = responseToJson(response);
+        assertEquals("Restaurant with id 67 not found", json.get("message"));
     }
 }
