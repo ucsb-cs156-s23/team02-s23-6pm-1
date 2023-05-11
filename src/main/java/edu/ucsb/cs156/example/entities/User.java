13fd2c700172109ed0e0be5792d17dd38b309d7b
Albert Yu <92063396+albertyu03@users.noreply.github.com>
2023-05-10 17:35:00 -0700

    Update User.java
    adding new line

--- src/main/java/edu/ucsb/cs156/example/entities/User.java
+++ src/main/java/edu/ucsb/cs156/example/entities/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
