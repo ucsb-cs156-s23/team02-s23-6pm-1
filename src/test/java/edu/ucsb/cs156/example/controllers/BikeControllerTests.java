8b62758d258be4b273c5e6d9dabca7f3b7841445
Max Fletcher <mfletcher@umail.ucsb.edu>
2023-05-11 15:09:21 -0700

    mf - Add DELETE Bike endpoint tests

--- src/test/java/edu/ucsb/cs156/example/controllers/BikeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/BikeControllerTests.java
@@ -181,4 +181,52 @@ public class BikeControllerTests extends ControllerTestCase {
                 String responseString = response.getResponse().getContentAsString();
                 assertEquals(expectedJson, responseString);
         }
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void admin_can_delete_a_bike() throws Exception {
+                // arrange
+
+                Bike bike = Bike.builder()
+                        .model("Among Us")
+                        .manufacturer("Innersloth")
+                        .numGears(69)
+                        .id(0L)
+                        .build();
+
+                when(bikeRepository.findById(eq(0L))).thenReturn(Optional.of(bike));
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                delete("/api/bikes?id=0")
+                                        .with(csrf()))
+                        .andExpect(status().isOk()).andReturn();
+
+                // assert
+                verify(bikeRepository, times(1)).findById(0L);
+                verify(bikeRepository, times(1)).delete(any());
+
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("Bike with id 0 deleted", json.get("message"));
+        }
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void admin_tries_to_delete_non_existant_bike_and_gets_right_error_message()
+                throws Exception {
+                // arrange
+
+                when(bikeRepository.findById(eq(15L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                delete("/api/bikes?id=15")
+                                        .with(csrf()))
+                        .andExpect(status().isNotFound()).andReturn();
+
+                // assert
+                verify(bikeRepository, times(1)).findById(15L);
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("Bike with id 15 not found", json.get("message"));
+        }
 }
